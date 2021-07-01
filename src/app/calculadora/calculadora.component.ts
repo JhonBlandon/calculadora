532c0570cf08c3e097a6818057dbe4522ddc78bb
tecnialarmas <tecnialarmas@iMac-de-tecnialarmas.local>
2021-07-01 18:29:10 -0500

    completa

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent  {
+
+  numero1: number=0;
+  numero2: number=0;
+  resultado: number=0;
+  
+suma(): void{
+  this.resultado = this.numero1 + this.numero2; 
+}
+resta(): void{
+  this.resultado = this.numero1 - this.numero2;
+}
+multiplicacion(): void{
+  this.resultado = this.numero1 * this.numero2;
+}
+division(): void{
+  this.resultado = this.numero1 / this.numero2;
+}
+}
